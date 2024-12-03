599be00b5d2c880535c2d67f7d8d851e056d93b4
darthpedroo <pedrovillarino06@gmail.com>
2024-12-03 12:08:54 -0300

    Added flask routes for wikipedia search

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import wiki as wiki_logic
+from mylib.logic import search_wiki
 
-result = wiki("Fanta Cake")
-print(result)
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API. Call /search or /wiki"}
+
+@app.get("/search/{value}")
+async def search(value:str):
+    """Page to search in wikipedia"""
+    
+    result = search_wiki(value)
+    return {"result": result}
+
+@app.get("/wiki/{value}")
+async def wiki(value:str):
+    """Retrieve summary from wikipedia page"""
+    result = wiki_logic(value)
+    return {"result":result}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host ="127.0.0.1")
+    
--- mylib/logic.py
+++ mylib/logic.py
@@ -2,10 +2,10 @@ import wikipedia
 
 
 def wiki(name:str, length=1):
-    """This is a wikipedia fetcher"""
-    
+    """This is a wikipedia fetcher"""    
     try:
         result = search_wiki(name)
+        print("result", result)
         first_result = result[0]
         my_wiki = wikipedia.summary(first_result, length)
         return my_wiki
