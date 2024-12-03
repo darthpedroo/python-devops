37f05d7a2d1ac8f4f844c86c8dbcd7ab602cca8c
darthpedroo <pedrovillarino06@gmail.com>
2024-12-03 01:07:02 -0300

    improved searching logic

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 [![Python application test with Github Actions](https://github.com/darthpedroo/python-devops/actions/workflows/devops.yml/badge.svg)](https://github.com/darthpedroo/python-devops/actions/workflows/devops.yml)
+
+How to use CLI tool:
+
+python .\cli-fire.py "Python" 1
--- mylib/logic.py
+++ mylib/logic.py
@@ -1,9 +1,18 @@
 import wikipedia
 
 
-def wiki(name="Fanta Cake", length=1):
+def wiki(name:str, length=1):
     """This is a wikipedia fetcher"""
+    
+    try:
+        result = search_wiki(name)
+        first_result = result[0]
+        my_wiki = wikipedia.summary(first_result, length)
+        return my_wiki
+    except Exception as ex:
+        print("Error:", ex)
 
-    my_wiki = wikipedia.summary(name, length)
-
-    return my_wiki
+def search_wiki(query: str):
+    """Search Wikipedia for Names"""
+    results = wikipedia.search(query)
+    return results
